03365af4937dcc52d557fc81cecf32e056a75506
Nicolas Marquette <nicolasmarquette.dev@gmail.com>
2024-10-05 00:24:55 +0200

    Labels

--- src/components/navbar.component.tsx
+++ src/components/navbar.component.tsx
@@ -7,21 +7,19 @@ export const NavBar = (): JSX.Element => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary" fixed="top">
       <Container>
-        <Navbar.Brand href="/home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/home">The Restaurant</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <Nav.Link href="/menu">Menu</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+            <NavDropdown title="Our history" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Since 1982</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Others</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
-                Separated link
+                Apply to our teams
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
