9754981fed9483805daab8eaa5860f8155db311d
Nicolas Marquette <nicolasmarquette.dev@gmail.com>
2024-10-04 23:22:11 +0200

    Add NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { NavBar } from "./components/navbar.component";
 
 function App() {
   return (
     <>
+      <NavBar />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<></>} />
