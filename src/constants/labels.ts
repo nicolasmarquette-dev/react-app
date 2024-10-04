58591654b2f2fc382958d72a94433da0ec97d1a8
Nicolas Marquette <nicolasmarquette.dev@gmail.com>
2024-10-05 01:07:33 +0200

    Add labels file

---
+++ src/constants/labels.ts
@@ -0,0 +1,14 @@
+const labels = {
+  en: {
+    APPLY: "Apply to our teams",
+    THE_RESTAURANT: "The Restaurant",
+    HOME: "Home",
+  },
+  fr: {
+    APPLY: "Postuler dans notre équipe",
+    THE_RESTAURANT: "Le Restaurant",
+    HOME: "Accueil",
+  },
+};
+
+export default labels;
